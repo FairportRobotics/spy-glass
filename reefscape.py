1d3ee8268c1accaf5261bb0eb79be288f67af3d0
Dom <manzaboy88657@gmail.com>
2025-02-06 20:03:58 -0500

    tested

--- reefscape.py
+++ reefscape.py
@@ -3,6 +3,6 @@ from ultralytics import YOLO
 # Load the weights from our repository
 model = YOLO("reefscape.pt")
 
-image_path = 'images/processed/ff4220bed1c921176c4471f3ff97095a.jpg'
+image_path = 'images/processed/0a28280f126e3c6d2ba0addc0051f89b.jpg'
 
 results = model(image_path, save_txt=True)
